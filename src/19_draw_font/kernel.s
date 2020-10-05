8b7cd227d30cd421a9aeb269f34d96015288ad70
nsasaki128 <naoki.robert.sasaki@gmail.com>
2020-10-05 17:07:08 +0900

    Add drawing font

---
+++ src/19_draw_font/kernel.s
@@ -0,0 +1,48 @@
+
+;**********************************************
+; Macro
+;**********************************************
+%include	"../include/define.s"
+%include	"../include/macro.s"
+
+	ORG	KERNEL_LOAD
+
+;**********************************************
+; Entry point
+;**********************************************
+[BITS 32]
+kernel:
+
+	;---------------------------------------
+	; Get font address
+	;---------------------------------------
+	mov	esi,	BOOT_LOAD + SECT_SIZE
+	movzx	eax,	word [esi + 0]
+	movzx	ebx,	word [esi + 2]
+	shl	eax,	4
+	add	eax,	ebx
+	mov	[FONT_ADR],	eax
+
+	;---------------------------------------
+	; Draw string
+	;---------------------------------------
+	cdecl	draw_font,	63,	13
+
+	;---------------------------------------
+	; Finish process
+	;---------------------------------------
+	jmp	$
+
+ALIGN 4,	db	0
+FONT_ADR:	dd	0
+;**********************************************
+; Moduel
+;**********************************************
+%include	"../modules/protect/vga.s"
+%include	"../modules/protect/draw_char.s"
+%include	"../modules/protect/draw_font.s"
+
+;**********************************************
+; Padding
+;**********************************************
+	times	KERNEL_SIZE - ($ - $$)	db	0
