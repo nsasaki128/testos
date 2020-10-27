87130a16058404de5f03967d63c881f915d782c1
nsasaki128 <naoki.robert.sasaki@gmail.com>
2020-10-27 19:51:23 +0900

    Add zero division

--- src/include/macro.s
+++ src/include/macro.s
@@ -19,3 +19,22 @@ struc drive
 	.head	resw	1
 	.sect	resw	1
 endstruc
+
+%macro	set_vect	1-*.nolit
+	push	eax
+	push	edi
+
+	mov	edi,	VECT_BASE + (%1 * 8)
+	mov	eax,	%2
+
+	%if 3 == %0
+		mov	[edi + 4],	%3
+	%endif
+
+	mov	[edi + 0],	ax
+	shr	eax,	16
+	mov	[edi + 6],	ax
+
+	pop	edi
+	pop	eax
+%endmacro
